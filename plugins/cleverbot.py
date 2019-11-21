20b6fecfad03f859fbe5601773ff81ccc7bda2bc
Jørn Lomax <jorn.lomax@arcticshores.com>
2019-11-21 13:48:50 +0000

    fix cleverbot?

--- plugins/cleverbot.py
+++ plugins/cleverbot.py
@@ -4,16 +4,16 @@ from plugins.base import Plugin
 from cleverbot import Cleverbot, CleverbotError
 
 
-class CelverBot(Plugin):
+class CleverBot(Plugin):
     commands = ["cb", "cleverbot"]
     cb = Cleverbot(os.environ.get("CLEVERBOT_API_TOKEN"), timeout=60)
 
     def message_recieved(self, command, message=""):    # pylint:disable=unused-argument
         data = {}
         try:
-            data["text"] = self.cb.say(" ".join(message)[1:]) or "No response"
+            data["text"] = self.cb.say(" ".join(message)) or "No response"
         except CleverbotError as error:
-            data["text"] = "Error connecting to clevebot: {}".format(error)
+            data["text"] = "Error connecting to cleverbot: {}".format(error)
         finally:
             return data
 
